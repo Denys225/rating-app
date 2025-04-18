fdbfcaf4270bda7746a7ea40c011a37d4b046cb8
Denys <denyskarpyshynua@gmail.com>
2025-04-18 17:26:23 +0300

    add something

--- Js.js
+++ Js.js
@@ -2,6 +2,7 @@ const submitBtn = document.getElementById("submitBtn");
 const ratingContainer = document.getElementById("ratingContainer");
 const thankYouContainer = document.getElementById("thankYouContainer");
 const ratingEvaluations = document.getElementById("ratingEvaluations");
+const ratingContent = document.querySelector(".rating-content");
 
 let selectedRating = null;
 
@@ -21,6 +22,18 @@ submitBtn.addEventListener('click', function () {
         thankYouContainer.style.display = 'flex';
         const text = document.getElementById("text");
         text.textContent = selectedRating;
+    } else {
+        if (!document.getElementById("error-message")) {
+            let erorText = document.createElement("erorText");
+            erorText.id = "error-message";
+            erorText.textContent = 'Make some choise';
+            erorText.style.color = 'hsl(25, 97%, 53%)';
+            erorText.style.fontSize = '20px'
+            erorText.style.display = 'flex';
+            erorText.style.justifyContent = 'center';
+            erorText.style.margin = '15px 0px 0px 0px';
+            ratingContent.appendChild(erorText)
+        }
     }
     
 });
